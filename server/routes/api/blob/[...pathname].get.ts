b294e6bf2663aa2ef4acb39892f8c479856b295f
jackgraddon <hello@jackgraddon.com>
2026-06-01 16:46:43 -0700

    Potential fix to blob path resolution

--- server/routes/api/blob/[...pathname].get.ts
+++ server/routes/api/blob/[...pathname].get.ts
@@ -1,12 +1,9 @@
-import { blob } from '@nuxthub/blob'
-
 export default defineEventHandler(async (event) => {
   const { pathname } = getRouterParams(event)
 
-  if (!pathname || !pathname.startsWith('photos/')) {
+  if (!pathname || !(pathname.startsWith('photos/') || pathname.startsWith('avatars/'))) {
     throw createError({ statusCode: 403, statusMessage: 'Forbidden' })
   }
 
-  // Use the modern blob SDK to serve the file
   return blob.serve(event, pathname)
 })
