9448773d29bf5dc6490aeaba56c69971557b2c9e
jackgraddon <hello@jackgraddon.com>
2026-06-02 08:35:08 -0700

    and the getter

--- server/api/user/me.get.ts
+++ server/api/user/me.get.ts
@@ -3,6 +3,7 @@ import { presignUrl } from '@vercel/blob'
 export default defineEventHandler(async (event) => {
   // Ensure the user is logged in
   const session = await requireUserSession(event)
+  const token = await getDelegationToken()
   
   const user = session.user
   let signedAvatarUrl = null
@@ -11,12 +12,13 @@ export default defineEventHandler(async (event) => {
   if (user.avatarPathname) {
     const token = await getDelegationToken()
     
-    signedAvatarUrl = presignUrl(token, {
-      pathname: user.avatarPathname,
-      operation: 'get',
+    const { presignedUrl } = await presignUrl(token, {
+      pathname: user.avatarUrl, // This is the key 'avatars/1-123.jpg'
       access: 'private',
+      operation: 'get',
       validUntil: Date.now() + 60 * 60 * 1000
     })
+    signedAvatarUrl = presignedUrl
   }
 
   return {
