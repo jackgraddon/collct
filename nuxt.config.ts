c34ddb4470f61ec6863dfc618af762a995010fc3
jackgraddon <hello@jackgraddon.com>
2026-07-16 20:27:42 -0700

    Add reference to webmanifest in head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,6 +72,7 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'manifest', href: '/manifest.webmanifest' },
         { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
         { rel: 'apple-touch-icon', sizes: '512x512', href: '/icon-512x512.png' },
       ],
